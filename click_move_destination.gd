33f72c7477bbefc0322bf7a6130d5461677d760d
Piyan0 <piyanapriyanto.0@gmail.com>
2026-05-23 09:13:09 +0700

    m

--- click_move_destination.gd
+++ click_move_destination.gd
@@ -9,7 +9,11 @@ var _routes:
         for i in mr_routes:
             r.append(i.global_position)
         return r
-        
+
+func _ready() -> void:
+    pass
+    
+    
 func get_routes(target_pos, click_pos):
     var routes_to_move = [click_pos]
     var available_routes = _routes.duplicate()
@@ -25,20 +29,21 @@ func get_routes(target_pos, click_pos):
                 distance = distance,
                 pos = route
             })
+            
         route_with_distance.sort_custom(func(a, b):
             return a.distance < b.distance
         )
-        var shortest_route = route_with_distance.back()
+        var shortest_route = route_with_distance.front()
         routes_to_move.append(shortest_route.pos)
         
-        for i in range(0, available_routes.size()):
+        for i in range(0, available_routes.size() - 1):
             if available_routes[i] == shortest_route.pos:
                 available_routes.pop_at(i)
         
         if shortest_route.pos == target_pos:
             break
-        
-    print(routes_to_move)
+    
+    routes_to_move.reverse()
     return routes_to_move
         
         
--- node_2d.gd
+++ node_2d.gd
@@ -4,11 +4,11 @@ extends Node2D
 @export var mr_click_pos: Node2D
 @export var player: Node2D
 @export var click_destination_pos: ClickMoveDestination
-
 func _ready():
-    await get_tree().create_timer(1).timeout
-    var x = click_destination_pos.get_routes(mr_target_pos.position, mr_click_pos.position)
+    await get_tree().create_timer(.2).timeout
+    var x = click_destination_pos.get_routes(player.position, mr_click_pos.position)
+    x.pop_front()
     for i in x:
         player.position = i
         print(1)
-        await get_tree().create_timer(1).timeout
+        await get_tree().create_timer(0.2).timeout
