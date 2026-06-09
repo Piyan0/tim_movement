bec1e2b01b0562a7637d926d85c40c5699329a6c
piyan0 <piyanapriyanto.0@gmail.com>
2026-06-09 11:58:43 +0700

    m

---
+++ ui/inventory_v2/dragable_item.gd
@@ -0,0 +1,26 @@
+extends Control
+
+var _drag = false
+var _drag_pos = Vector2.ZERO
+
+
+func _process(delta):
+    position = lerp(position, _drag_pos, 0.2)
+    
+    
+func _gui_input(event: InputEvent):
+    if event is InputEventMouseButton:
+        if event.button_index = MOUSE_BUTTON_LEFT && event.is_pressed():
+            _drag = true
+            _drag_pos = event.position
+    
+    if event is InputEventMouseMotion:
+        _drag_pos = event.position
+
+
+func _input(event: InputEvent):
+    if event is InputEventMouseButton:
+        if event.button_index = MOUSE_BUTTON_LEFT && !event.is_pressed():
+            _drag = false
+
+    
