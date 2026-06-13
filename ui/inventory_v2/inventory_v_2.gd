517776969893f5702c31969490b614ba1b364137
Piyan0 <piyanapriyanto.0@gmail.com>
2026-06-13 13:10:38 +0700

    Commit at, Jun 13 2026.

--- ui/inventory_v2/inventory_v_2.gd
+++ ui/inventory_v2/inventory_v_2.gd
@@ -8,14 +8,15 @@ const _item_picked_drop = 100
 const _item_picked_tween_dur = 0.2 
 
 func _ready() -> void:
-    
     for slot in _items_slot:
         slot.item_picked.connect(func():
+            mouse_behavior_recursive = Control.MOUSE_BEHAVIOR_DISABLED
             var t = create_tween().set_parallel().set_trans(Tween.TRANS_BACK)
             t.tween_property(self, "position:y", position.y + _item_picked_drop, _item_picked_tween_dur)
-            t.tween_property(self, "modulate:a", 0.5, _item_picked_tween_dur)
+            t.tween_property(self, "modulate:a", 0.0, _item_picked_tween_dur)
         )
         slot.item_dropped.connect(func():
+            mouse_behavior_recursive = Control.MOUSE_BEHAVIOR_ENABLED
             var t = create_tween().set_parallel().set_trans(Tween.TRANS_BACK)
             t.tween_property(self, "modulate:a", 1, _item_picked_tween_dur)
             t.tween_property(self, "position:y", position.y + (-_item_picked_drop), _item_picked_tween_dur)
