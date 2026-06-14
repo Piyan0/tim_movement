17bafa246d917869853511df8894fa2518f2e287
Piyan0 <piyanapriyanto.0@gmail.com>
2026-06-14 20:32:07 +0700

    Commit at, Jun 14 2026.

--- levels/main/player_click_area.gd
+++ levels/main/player_click_area.gd
@@ -5,3 +5,7 @@ func _gui_input(event: InputEvent) -> void:
     if event is InputEventMouseButton:
         if event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
             Player.instance.move_to_click_pos(event.position)
+    elif event is InputEventScreenTouch:
+        if event.pressed:
+            Player.instance.move_to_click_pos(event.position)
+
